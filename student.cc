5da2fc34fdf6ca33194e45834495a1e94a368e76
mroukema <mroukema@gmail.com>
2013-11-30 14:02:15 -0500

    Commiting work completed so far on student implemenation

--- student.cc
+++ student.cc
@@ -1,10 +1,16 @@
 #include "student.h"
+#include "watcard.h"
 
 Student::Student( Printer &prt, NameServer &nameServer, WATCardOffice &cardOffice, unsigned int id,
         unsigned int maxPurchases ) :
-    printer(prt), server(nameServer), office(cardOffice)
+    printer(prt), server(nameServer), office(cardOffice), id(id), maxPurchases(maxPurchases)
 {
 }
         
-void Student::main() {
+void Student::main() 
+{
+    unsigned int numPurchases = mprand( 1, maxPurchases );
+    unsigned int flavour = mprand( 3 ); //
+    WATCard::FWATCard card = office.create( id, 5 );
+    server.getMachine( id );
 }
--- student.h
+++ student.h
@@ -4,14 +4,19 @@
 #include "printer.h"
 #include "nameserver.h"
 #include "watcard.office.h"
+#include "watcard.h"
+#include <uFuture.h>
 
 _Task Student {
     Printer &printer;
     NameServer &server;
     WATCardOffice &office;
+    const unsigned int id;
+    const unsigned int maxPurchases;
     
     void main();
   public:
+    typedef Future_ISM<WATCard *> FWATCard;
     Student( Printer &prt, NameServer &nameServer, WATCardOffice &cardOffice, unsigned int id,
              unsigned int maxPurchases );
 };
